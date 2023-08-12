7a2cd3a460bfdcb596b36d26c3e04d8d9ea2bbe7
thiago.melo <thiago.melo@govbr.com.br>
2023-08-12 02:22:55 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/dynns/cloudtecnologia/logistica/MaximaLogisticaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximaLogisticaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaximaLogisticaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynns/cloudtecnologia/logistica/MaximaLogisticaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaximaLogisticaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
