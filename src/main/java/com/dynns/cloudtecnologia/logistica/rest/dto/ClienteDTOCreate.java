a28989743d40f7ef53cd6c05bb275fb41c5b6ff9
thiago.melo <thiago.melo@govbr.com.br>
2023-08-13 02:11:55 -0300

    Ajustes

--- src/main/java/com/dynns/cloudtecnologia/logistica/rest/dto/ClienteDTOCreate.java
+++ src/main/java/com/dynns/cloudtecnologia/logistica/rest/dto/ClienteDTOCreate.java
@@ -9,36 +9,36 @@ import javax.validation.constraints.*;
 @Data
 public class ClienteDTOCreate {
 
-    @NotBlank(message = "O nome deve ser informado.")
+    @NotBlank(message = "O nome deverá ser informado.")
     private String nome;
 
-    @NotBlank(message = "O cnpj deve ser informado.")
-    @Size(min = 14, max = 14, message = "O CNPJ deve ter 14 caracteres.")
-    @Pattern(regexp = "\\d{14}", message = "O CNPJ deve conter apenas números.")
+    @NotBlank(message = "O cnpj deverá ser informado.")
+    @Size(min = 14, max = 14, message = "O CNPJ deverá conter 14 caracteres.")
+    @Pattern(regexp = "\\d{14}", message = "O CNPJ deverá conter apenas números.")
     @CNPJunico
     private String cnpj;
 
-    @NotBlank(message = "O cep deve ser informado.")
-    @Size(min = 8, max = 8, message = "O cep deve ter 8 caracteres.")
-    @Pattern(regexp = "\\d{8}", message = "O cep deve conter apenas números.")
+    @NotBlank(message = "O cep deverá ser informado.")
+    @Size(min = 8, max = 8, message = "O cep deve conter 8 caracteres.")
+    @Pattern(regexp = "\\d{8}", message = "O cep deverá conter apenas números.")
     private String cep;
 
-    @NotBlank(message = "O logradouro deve ser informado.")
+    @NotBlank(message = "O logradouro deverá ser informado.")
     private String logradouro;
 
-    @NotBlank(message = "O bairro deve ser informado.")
+    @NotBlank(message = "O bairro deverá ser informado.")
     private String bairro;
 
-    @NotBlank(message = "A localidade deve ser informada.")
+    @NotBlank(message = "A localidade deverá ser informada.")
     private String localidade;
 
-    @NotBlank(message = "A uf deve ser informada.")
-    @Size(min =2 , max = 2, message = "A uf 2 deve conter 2 caracteres.")
+    @NotBlank(message = "A uf deverá ser informada.")
+    @Size(min =2 , max = 2, message = "A uf 2 deverá conter 2 caracteres.")
     private String uf;
 
-    @NotBlank(message = "O Campo latitude não poderá ser nulo.")
+    @NotBlank(message = "O Campo latitude não pode ser nulo.")
     private String latitude;
 
-    @NotBlank(message = "O Campo longitude não poderá ser nulo.")
+    @NotBlank(message = "O Campo longitude não pode ser nulo.")
     private String longitude;
 }
